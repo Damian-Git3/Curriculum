ce2342862a27e677ce3d07a8f01056bb9a47264c
Damian <johndoe@example.com>
2024-05-26 17:14:02 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/ExperienciaLaboral.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Curriculum.Controllers
+{
+    public class ExperienciaLaboral : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/PreparacionAcademica.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Curriculum.Controllers
+{
+    public class PreparacionAcademica : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
